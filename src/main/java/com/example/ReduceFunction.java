c64835ec63f98a5b456079851dd9ac7d8d4eff61
MikeQin <mike.qin@gmail.com>
2019-07-19 23:15:19 -0400

    init

--- src/main/java/com/example/ReduceFunction.java
+++ src/main/java/com/example/ReduceFunction.java
@@ -24,24 +24,24 @@ public class ReduceFunction {
 		// U reduce(I, (U, T) -> U, (U, U) -> U)
 		Map<String, List<Person>> initMap = new HashMap<>();
 		Map<String, List<Person>> result = persons
-				.stream()
-				.map(person -> {
-					person.setUuid(UUID.randomUUID());
-					return person;
-				})
-				.reduce(initMap, (resultMap, person) -> {
-					if (resultMap.get(person.getOccupation()) == null) {
-						List<Person> list = new ArrayList<>();
-						list.add(person);
-						resultMap.put(person.getOccupation(), list);
-					} else {
-						resultMap.get(person.getOccupation()).add(person);
-					}
-					return resultMap;
-				}, (x, y) -> {
-					// Combiner, nothing to do here... simple pass through
-					return y;
-				});
+			.stream()
+			.map(person -> {
+				person.setUuid(UUID.randomUUID());
+				return person;
+			})
+			.reduce(initMap, (resultMap, person) -> {
+				if (resultMap.get(person.getOccupation()) == null) {
+					List<Person> list = new ArrayList<>();
+					list.add(person);
+					resultMap.put(person.getOccupation(), list);
+				} else {
+					resultMap.get(person.getOccupation()).add(person);
+				}
+				return resultMap;
+			}, (x, y) -> {
+				// Combiner, nothing to do here... simple pass through
+				return y;
+			});
 		
 		System.out.println("\nCategories by Occupation:\n" + Utils.toJson(result));
 		
@@ -89,30 +89,30 @@ public class ReduceFunction {
 		initMap.put(age40to50, new ArrayList<Person>());
 		
 		Map<String, List<Person>> result = persons
-				.stream()
-				.map(person -> {
-					person.setUuid(UUID.randomUUID());
-					return person;
-				})				
-				.reduce(initMap, (accumMap, person) -> {
-			
-					if (person.getAge()>= 20 && person.getAge() < 30) {
-						accumMap.get(age20to30).add(person);
-					}
-					else if (person.getAge()>= 30 && person.getAge() < 40) {
-						accumMap.get(age30to40).add(person);
-					}
-					else if (person.getAge()>= 40 && person.getAge() < 50) {
-						accumMap.get(age40to50).add(person);
-					}
-					else {
-						throw new RuntimeException("Age is beyond the range ...");
-					}
-					
-					return accumMap;
-					
-				}, (x, y) -> (y));
+			.stream()
+			.map(person -> {
+				person.setUuid(UUID.randomUUID());
+				return person;
+			})				
+			.reduce(initMap, (accumMap, person) -> {
 		
+				if (person.getAge()>= 20 && person.getAge() < 30) {
+					accumMap.get(age20to30).add(person);
+				}
+				else if (person.getAge()>= 30 && person.getAge() < 40) {
+					accumMap.get(age30to40).add(person);
+				}
+				else if (person.getAge()>= 40 && person.getAge() < 50) {
+					accumMap.get(age40to50).add(person);
+				}
+				else {
+					throw new RuntimeException("Age is beyond the range ...");
+				}
+				
+				return accumMap;
+				
+			}, (x, y) -> (y));
+	
 		System.out.println("\nAge Groups:\n" + Utils.toJson(result));
 		
 		return result;
@@ -120,13 +120,13 @@ public class ReduceFunction {
 	
 	public List<Person> filterByAge(int age) {
 		List<Person> result = persons
-				.stream()
-				.map(person -> {
-					person.setUuid(UUID.randomUUID());
-					return person;
-				})
-				.filter(p -> (p.getAge() > age))
-				.collect(Collectors.toList()); 
+			.stream()
+			.map(person -> {
+				person.setUuid(UUID.randomUUID());
+				return person;
+			})
+			.filter(p -> (p.getAge() > age))
+			.collect(Collectors.toList()); 
 		// Terminal operations: reduce(), collect(), sum(), average(), count()
 		
 		System.out.println("\nFilter By Age "+ age +":\n" + Utils.toJson(result));
@@ -136,13 +136,13 @@ public class ReduceFunction {
 	
 	public long countByOccupation(String occupation) {
 		long result = persons
-				.stream()
-				.map(person -> {
-					person.setUuid(UUID.randomUUID());
-					return person;
-				})
-				.filter(p -> (p.getOccupation() == occupation))
-				.count();
+			.stream()
+			.map(person -> {
+				person.setUuid(UUID.randomUUID());
+				return person;
+			})
+			.filter(p -> (p.getOccupation() == occupation))
+			.count();
 		
 		String formatted = String.format("\n%s Count: %d", occupation, result);
 		System.out.println(formatted);
